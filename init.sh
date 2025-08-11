4117e339f4cbf21ac77acabdd8ff283683d8a1c4
suku-7 <ryumon6550@gmail.com>
2025-08-11 11:04:33 +0900

    gitignore 설정 두번째, application.yml 수정

--- init.sh
+++ init.sh
@@ -1,20 +1,22 @@
 # ========================================
 # FILENAME: aivlebigproject/init.sh
-# 역할 : 
+# 역할 : 로컬 환경 초기 패키지 설치.
 # ========================================
 
+# 1. 시스템 기본 패키지 설치
 sudo apt-get update
 sudo apt-get install net-tools
 sudo apt install iputils-ping
 sudo apt install httpie
-# pip install httpie
 
+# 2. Kubernetes CLI (kubectl) 설치
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
+# 3. Node.js 버전 관리자 (nvm) 및 Node.js 설치
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 14.19.0 && nvm use 14.19.0
 export NODE_OPTIONS=--openssl-legacy-provider
 
-# docker-compose up kafka
+echo "✅ Initialization complete!"
